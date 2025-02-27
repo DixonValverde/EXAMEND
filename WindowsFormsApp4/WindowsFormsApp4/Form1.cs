dac2e6acadf039fa176c650e42c6e98b290ba82c
Boris Dixon <164239667+DixonValverde@users.noreply.github.com>
2025-02-26 21:45:04 -0500

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using WindowsFormsApp4;
+
+namespace GestionCalificaciones
+{
+    public partial class Form1 : Form
+    {   //lista de alumnos
+        private List<alumno> alumnos = new List<alumno>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnagg_Click(object sender, EventArgs e)
+        {
+            string dni = dn.Text;
+            string nombre = nom.Text;
+            string apellido = ape.Text;
+            if (!int.TryParse(notas.Text, out int nota))
+            {
+                MessageBox.Show("Ingrese una nota válida.");
+                return;
+            }
+            //creamos si el alumno tiene ya un dni registrado
+            if (alumnos.Any(a => a.dni == dni))
+            {
+                MessageBox.Show("El alumno con este DNI ya existe D:");
+                return;
+            }
+
+            alumno nuevoAlumno = new alumno(nombre, apellido, dni, nota);
+            alumnos.Add(nuevoAlumno);
+            cali.Text = nuevoAlumno.calificacion; 
+            MessageBox.Show("Alumno agregado correctamente.");
+        }
+
+        private void btnAprobados_Click(object sender, EventArgs e)
+        {// si es mauor a 7 aprueba
+            var aprobados = alumnos.Where(a => a.nota >=7).ToList();
+            MostrarLista(alumnos.Where(a => a.nota >=7).ToList(), "Alumnos Aprobados");
+        }
+
+        private void btnSuspendidos_Click(object sender, EventArgs e)
+        {   //si es menor a 6.99
+            var suspendidos = alumnos.Where(a => a.nota < 6.99).ToList();
+            MostrarLista(alumnos.Where(a => a.nota < 6.99).ToList(), "Alumnos Suspendidos");
+        }
+        //si no hay alumnos en los campos
+        private void MostrarLista(List<alumno> lista, string titulo)
+        {
+            string mensaje = lista.Count > 0 ? string.Join("\n", lista) : "No hay alumnos alumnos en esta seccion.";
+            MessageBox.Show(mensaje, titulo);
+        }
+    }
+}
+
